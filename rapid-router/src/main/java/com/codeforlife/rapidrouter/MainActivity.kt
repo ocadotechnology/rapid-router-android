4230b6db15afe610d94543e8f99a75c44a3fb950
charafau <gorudonu@gmail.com>
2018-03-16 13:41:39 +0000

    Added libgdx fragment as game view

--- rapid-router/src/main/java/com/codeforlife/rapidrouter/MainActivity.kt
+++ rapid-router/src/main/java/com/codeforlife/rapidrouter/MainActivity.kt
@@ -3,13 +3,21 @@ package com.codeforlife.rapidrouter
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import com.badlogic.gdx.backends.android.AndroidFragmentApplication
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AndroidFragmentApplication.Callbacks {
+    override fun exit() {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout)
 
         val pager = findViewById<ViewPager>(R.id.view_pager)
         pager.adapter = MainViewPagerAdapter(supportFragmentManager)
+
+        supportActionBar?.elevation = 0.0f
+
     }
 }
