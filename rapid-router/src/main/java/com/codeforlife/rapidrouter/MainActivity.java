36cc1e6dc8c99d3a31b4048b655b8c2504ac3785
Jacq Train <jacqtrain@gmail.com>
2018-03-16 12:55:42 +0000

    Introduce tab view for Blockly and Game fragments

--- rapid-router/src/main/java/com/codeforlife/rapidrouter/MainActivity.java
+++ rapid-router/src/main/java/com/codeforlife/rapidrouter/MainActivity.java
@@ -1,9 +1,11 @@
 package com.codeforlife.rapidrouter;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewParent;
 
 import com.google.blockly.android.AbstractBlocklyActivity;
 import com.google.blockly.android.codegen.CodeGenerationRequest;
@@ -14,43 +16,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AbstractBlocklyActivity {
-    private final CodeGenerationRequest.CodeGeneratorCallback generatorCallback =
-            new LoggingCodeGeneratorCallback(this, "LoggingTag");
-
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
+        setContentView(R.layout.layout);
 
-    @Override
-    protected View onCreateContentView(int parentId) {
-        return getLayoutInflater().inflate(R.layout.layout, null);
-    }
-
-    @NonNull
-    @Override
-    protected String getToolboxContentsXmlPath() {
-        return "toolbox/toolbox37.xml";
-    }
-
-    @NonNull
-    @Override
-    protected List<String> getBlockDefinitionsJsonPaths() {
-        final ArrayList<String> assetPaths = new ArrayList<>(DefaultBlocks.getAllBlockDefinitions());
-        assetPaths.add("blocks/CustomBlocks.json");
-        return assetPaths;
-    }
-
-    @NonNull
-    @Override
-    protected List<String> getGeneratorsJsPaths() {
-        return Collections.emptyList();
-    }
-
-    @NonNull
-    @Override
-    protected CodeGenerationRequest.CodeGeneratorCallback getCodeGenerationCallback() {
-        return generatorCallback;
+        ViewPager pager = findViewById(R.id.view_pager);
+        pager.setAdapter(new MainViewPagerAdapter(getSupportFragmentManager()));
     }
 }
