27718614f24273a00f3c3a84081e009301aebaad
charafau <gorudonu@gmail.com>
2018-03-21 15:34:53 +0000

    Added sample spek test Added spek dependencies

---
+++ rapid-router/src/test/java/com/codeforlife/rapidrouter/gdx/RoadBuilderTest.kt
@@ -0,0 +1,16 @@
+package com.codeforlife.rapidrouter.gdx
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.junit.Assert.assertEquals
+
+class RoadBuilderTest : Spek({
+    describe("A road builder") {
+
+        it("should be true") {
+            assertEquals(true, true)
+        }
+
+    }
+})
